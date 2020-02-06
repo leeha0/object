6563375df1815fea881956ae738f226b1ba69340
leeha0 <gkdud2837@gmail.com>
2020-02-06 23:37:48 +0900

    Chapter4 - 설계 품질과 트레이드 오프: 데이터 중심 설계

---
+++ src/main/java/chapter4/domain/Movie.java
@@ -0,0 +1,56 @@
+package chapter4.domain;
+
+import chapter2.domain.Money;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Movie {
+    private String titile;
+    private Money fee;
+    private List<DiscountCondition> discountConditions;
+
+    private MovieType movieType;
+    private Money discountAmount;
+    private double discountPercent;
+
+    public MovieType getMovieType() {
+        return movieType;
+    }
+
+    public void setMovieType(MovieType movieType) {
+        this.movieType = movieType;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public void setFree(Money bee) {
+        this.fee = fee;
+    }
+
+    public List<DiscountCondition> getDiscountConditions() {
+        return Collections.unmodifiableList(discountConditions);
+    }
+
+    public void setDiscountConditions(List<DiscountCondition> discountConditions) {
+        this.discountConditions = discountConditions;
+    }
+
+    public Money getDiscountAmount() {
+        return this.discountAmount;
+    }
+
+    public void setDiscountAmount(Money discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public double getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public void setDiscountPercent(double discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+}
---
+++ src/main/java/chapter4/domain/MovieType.java
@@ -0,0 +1,7 @@
+package chapter4.domain;
+
+public enum MovieType {
+    AMOUNT_DISCOUNT,
+    PERCENT_DISCOUNT,
+    NON_DISCOUNT
+}
